89dfaefe7fb4e85c74ded9d6346e92f58af50afc
rafal19987 <rafal987.1@o2.pl>
2023-02-19 11:28:07 +0100

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 
 const App = () => {
   return (
-    <div>
-      <Home></Home>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </>
   );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Button from '../../components/Button/Button';
+import './Home.scss';
 
 const Home = () => {
-  return <div>This is home Page</div>;
+  return (
+    <div className="container">
+      <h1>This is JS interview questions App</h1>
+      <p>You can improve your knowleage trying to qnsware for all questions.</p>
+      <p>
+        Don`t warry if you will don`t now answers for all questions. This app is for help you to
+        train and makes you gigaChad
+      </p>
+      <p>For start click the button below</p>
+      <Button />
+    </div>
+  );
 };
 
 export default Home;
